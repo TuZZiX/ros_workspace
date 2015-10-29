0fb93b9dafec7e21ab03a9e211561f3c6e3cdcfe
TuZZiX <tianshipei5@126.com>
2015-10-29 13:55:28 -0400

    update

--- src/my_interesting_moves/include/my_interesting_moves/my_interesting_moves.hpp
+++ src/my_interesting_moves/include/my_interesting_moves/my_interesting_moves.hpp
@@ -17,16 +17,19 @@
 class Baxter_right_arm
 {
 public:
-	bool enable_pose_limit = false;
+	bool enable_pose_limit = false;		//make this true will force limit check for every pose to move and restrict if out of limit
 	///  class constructor, create and connect to the action client to send trajectory commander
 	Baxter_right_arm(ros::NodeHandle* nodehandle)
 	{
 		bool server_exists;
+		// because the action client and traj steamer need to be initalize with parameter, so it could not done outside function
+		// so I initalize it in constructor, and use a pointer get it out.
 		static actionlib::SimpleActionClient<baxter_traj_streamer::trajAction> action_client("trajActionServer", true);
 		static Baxter_traj_streamer baxter_traj_streamer(nodehandle);
-		g_cnt = 0;
 		traj_streamer = &baxter_traj_streamer;
 		as_ = &action_client;
+		
+		g_cnt = 0;
 		ROS_INFO("Baxter_right_arm: waiting for server: ");
 		while (ros::ok())
 		{	//keep trying when failed
@@ -43,170 +46,195 @@ public:
 		}
 	}
 	///  move right arm to a certain pose, waiting for finish
+	///  matrix overload version
 	///
-	///  @param joint_pose input:
+	///  @param joint_pose input: the pose to reach
 	void move(Vectorq7x1 joint_pose)
 	{
-		add_movement(joint_pose);
-		start_move();
-		wait_for_finish();
+		add_movement(joint_pose);	//add to move sequence
+		start_move();				//start moving
+		wait_for_finish();			//and wait
 	}
 	///  move right arm to a certain pose, waiting for finish
+	///  array overload version
 	///
-	///  @param joint_pose input:
+	///  @param joint_pose input: the pose to reach
 	void move(const double joint_pose[])
 	{
-		add_movement(joint_pose);
-		start_move();
-		wait_for_finish();
+		add_movement(joint_pose);	//add to move sequence
+		start_move();				//start moving
+		wait_for_finish();			//and wait
 	}
-	///  <#Description#>
+	///  add a pose to the movement sequence, once start moving, the arm will move over every pose in the sequence with respect of order
+	///  you need to call start_move and wait_for_finish to apply the poses
+	///  matrix overload version
 	///
-	///  @param joint_pose <#joint_pose description#>
+	///  @param joint_pose input: the next pose to add to the movement sequence
 	void add_movement(Vectorq7x1 joint_pose)
 	{
 		ROS_INFO("Baxter_right_arm: pose [%f, %f, %f, %f, %f, %f, %f] added to the sequence", 
 			joint_pose[0], joint_pose[1], joint_pose[2], joint_pose[3], 
-			joint_pose[4], joint_pose[5], joint_pose[6]);
-		check_limit(joint_pose);
-		ros::spinOnce();
-		if (first_cmd == true)
+			joint_pose[4], joint_pose[5], joint_pose[6]);		//display the pose
+		check_limit(joint_pose);				//restrict if out of limit
+		ros::spinOnce();						//wait for feedback callback, so that the current arm pose will be updated
+		if (first_cmd == true)					//if this is the first pose in the sequence, add the current pose of arm before it
 		{
-			q_vec_right_arm = (*traj_streamer).get_qvec_right_arm();
-			q_in_vecxd = q_vec_right_arm; // start from here;
-			des_path.push_back(q_vec_right_arm); //put all zeros here
-			first_cmd = false;
+			q_vec_right_arm = (*traj_streamer).get_qvec_right_arm();	//get current arm pose
+			q_in_vecxd = q_vec_right_arm;								//convert to Eigen Vector
+			des_path.push_back(q_vec_right_arm);						//makes it the first pose
+			first_cmd = false;											//so this work won't be done next time
 		}
-		q_in_vecxd = joint_pose; // conversion; not sure why I needed to do this...but des_path.push_back(q_in_vecxd) likes it
-		des_path.push_back(joint_pose); //twice, to define a trajectory
+		q_in_vecxd = joint_pose;			//convert to Eigen Vector
+		des_path.push_back(joint_pose);		//add customed trajectory to sequence
 	}
-	///  <#Description#>
+	///  add a pose to the movement sequence, once start moving, the arm will move over every pose in the sequence with respect of order
+	///  you need to call start_move and wait_for_finish to apply the poses
+	///  array overload version
 	///
-	///  @param joint_pose <#joint_pose description#>
+	///  @param joint_pose input: the next pose to add to the move sequence
 	void add_movement(const double joint_pose[])
 	{
-		Vectorq7x1 pose;
-		for (int i = 0; i < 7; i++) { //pre-size these vectors, so can access w/ indices
-			pose[i] = joint_pose[i];
+		Vectorq7x1 pose;				//construct a matrix
+		for (int i = 0; i < 7; i++) {
+			pose[i] = joint_pose[i];	//copy numbers in array to matrix
 		}
-		add_movement(pose);
+		add_movement(pose);				//call matrix version to execute
 	}
-///  <#Description#>
+	///  stop the unfinishded movement, and start moving arm to the pose in current sequence
+	///  move sequence will be cleared once called
 	void start_move()
 	{
+		(*as_).cancelGoal();			//if there is a unfinished goal, stop it
 		(*traj_streamer).stuff_trajectory(des_path, joint_traj); //convert from vector of 7dof poses to trajectory message
-		des_path.clear();
-		first_cmd = true;
-		goal_.trajectory = joint_traj;
-		goal_.traj_id = g_cnt;
-		g_cnt++;
-		(*as_).sendGoal(goal_);
-		while (check_as_state().compare("ACTIVE") != 0);
+		des_path.clear();				//clear the sequence
+		first_cmd = true;				//also clear the flag
+		goal_.trajectory = joint_traj;	//fill the goal
+		goal_.traj_id = g_cnt++;		//also increase the goal count
+		(*as_).sendGoal(goal_);			//sent goal to interpolator_as to start moving
+		while (check_as_state().compare("ACTIVE") != 0);	//make sure the goal have been accepted
 		ROS_INFO("Baxter_right_arm: goal number %d accepted", g_cnt);
 	}
-///  <#Description#>
+///  stop current movement
 	void stop_move()
 	{
-		(*as_).cancelGoal();
+		(*as_).cancelGoal();			//tell action server the goal have been canceled
 		ROS_INFO("Baxter_right_arm: goal number %d cancelled", g_cnt);
 	}
-	///  <#Description#>
+	///  check current action server state
+	///  state list:
+	///  PENDING
+	///  ACTIVE
+	///  RECALLED
+	///  REJECTED
+	///  PREEMPTED
+	///  ABORTED
+	///  SUCCEEDED
+	///  LOST
 	///
-	///  @return <#return value description#>
+	///  @return string of current action server state, compare it with state above
 	std::string check_as_state()
 	{
-		return ((*as_).getState()).toString();
+		return ((*as_).getState()).toString();	//return the string of action server state
 	}
-	///  <#Description#>
+	///  wait for the current movement to finish in a certain time
 	///
-	///  @param .0 <#.0 description#>
+	///  @param input: the time (in second) to wait, 0 = unlimited time, default is 0
 	///
-	///  @return <#return value description#>
+	///  @return is movement finish on time
 	bool wait_for_finish(double timeout = 0.0)
 	{
-		if (timeout <= 0.0)
+		if (timeout <= 0.0)				//for infinite time
 		{
 			(*as_).waitForResult();
 		}
 		else
 		{
-			bool finished_before_timeout = (*as_).waitForResult(ros::Duration(timeout));
+			bool finished_before_timeout = (*as_).waitForResult(ros::Duration(timeout));	//wait before timeout
 			if (!finished_before_timeout) {
 				ROS_WARN("Baxter_right_arm: giving up waiting on result for goal number %d", g_cnt);
-				return false;
+				return false;			//did not finished on time
 			}
 		}
 		ROS_INFO("Baxter_right_arm: goal number %d finished before timeout", g_cnt);
-		return true;
+		return true;					//finished
 	}
-	///  <#Description#>
+	///  move hand to front and start waving  hand
 	///
-	///  @param times <#times description#>
+	///  @param times input: how many times you want to wave, default is 1
 	void wave_hand(int times = 1)
 	{
 		for (int i = 0; i < times; i++)
 		{
-			move(wave_pose_right[1]);
-			move(wave_pose_right[2]);
-			move(wave_pose_right[1]);
-			move(wave_pose_right[0]);
-			move(wave_pose_right[1]);
+			move(wave_pose_right[1]);	//middle
+			move(wave_pose_right[2]);	//right
+			move(wave_pose_right[1]);	//middle
+			move(wave_pose_right[0]);	//left
+			move(wave_pose_right[1]);	//middle
 		}
 	}
+	///  let robot to push the beer off the table
 	void push_beer()
 	{
-		move(push_pose_right[0]);
+		move(push_pose_right[0]);		//send pose sequence
 		move(push_pose_right[1]);
 		move(push_pose_right[2]);
 	}
+	///  get arm above the table, so it will not stuck especially at inital pose
 	void move_above_table()
 	{
-		move(table_pose_right[0]);
+		move(table_pose_right[0]);		//send pose sequence
 		move(table_pose_right[1]);
 	}
 private:
-	const double wave_pose_right[3][7] = {
+	const double wave_pose_right[3][7] = {		//pose sequence for wave hand
 		{0.5, 0, 0, 0, 0.5, -1.74, 0},
 		{0.5, 0, 0, 0, 0, -1.74, 0},
 		{0.5, 0, 0, 0, -0.5, -1.74, 0}
 	};
-	const double push_pose_right[3][7] = {
+	const double push_pose_right[3][7] = {		//pose sequence for push the beer
 		{0.7, 0.2, 0.4, 0.7, -0.3, -0.7, 0},
 		{1.0, 0.2, 0.4, 0.7, -0.3, -0.7, 0},
 		{1.3, 0.2, 0.4, 0.7, -0.3, -0.7, 0}
 	};
-	const double table_pose_right[2][7] = {
+	const double table_pose_right[2][7] = {		//pose sequence for get hand on table
 		{-0.6, 0.6, 0, -0.4, 0, -1, 0},
 		{0.5, 0.2, 0, 0.4, 0, -0.7, 0}
 	};
-	const double pose_limit[2][7] = {
+	const double pose_limit[2][7] = {			//max angle for each pose
 		{M_PI/2, M_PI/4, M_PI/2, M_PI/4, M_PI/2, M_PI/4, M_PI/2,},			//from position in col 1 to position in col 2
 		{-M_PI/2, -M_PI/4, -M_PI/2, -M_PI/6, -M_PI/2, -M_PI/4, -M_PI/2,}
 	};
-	actionlib::SimpleActionClient<baxter_traj_streamer::trajAction> *as_;
-	baxter_traj_streamer::trajGoal goal_;
-	trajectory_msgs::JointTrajectory joint_traj;
-	Baxter_traj_streamer *traj_streamer;
-	int g_cnt;
-	bool first_cmd = true;
-	Eigen::VectorXd q_in_vecxd;
-	Vectorq7x1 q_vec_right_arm;
-	std::vector<Eigen::VectorXd> des_path;
 	
+	actionlib::SimpleActionClient<baxter_traj_streamer::trajAction> *as_;	//point to action client
+	baxter_traj_streamer::trajGoal goal_;									//goal of interpolator_as
+	trajectory_msgs::JointTrajectory joint_traj;							//trajectory form of arm pose
+	Baxter_traj_streamer *traj_streamer;									//point to traj streamer
+	int g_cnt;																//goal counter
+	bool first_cmd = true;													//first pose flag
+	Eigen::VectorXd q_in_vecxd;												//use to convert value to vector
+	Vectorq7x1 q_vec_right_arm;												//use to store current arm pose
+	std::vector<Eigen::VectorXd> des_path;									//pose sequence
+	
+	///  check angle of each pose, make sure no angle is out of limitation
+	///  if the angle is out of limit, it will be adjust to the max possible value
+	///
+	///  @param input&output: joint_pose the pose to check
+	///
+	///  @return is any angle out of limit
 	bool check_limit(Vectorq7x1 &joint_pose)
 	{
-		bool all_correct = true;
+		bool all_correct = true;											//is no angle out of limit?
 		if (enable_pose_limit == true)
 		{
 			for (int i = 0; i < 7; i++)
 			{
-				if (joint_pose[i] > pose_limit[0][i])
+				if (joint_pose[i] > pose_limit[0][i])						//check upper limit
 				{
-					joint_pose[i] = pose_limit[0][i];
+					joint_pose[i] = pose_limit[0][i];						//adjust the angle
 					ROS_WARN("joint_pose[i] is above upper limit");
 					all_correct = false;
 				}
-				if (joint_pose[i] < pose_limit[1][i])
+				if (joint_pose[i] < pose_limit[1][i])						//check lower limit
 				{
 					joint_pose[i] = pose_limit[1][i];
 					ROS_WARN("joint_pose[i] is below lower limit");
@@ -217,6 +245,7 @@ private:
 			joint_pose[0], joint_pose[1], joint_pose[2], joint_pose[3], 
 			joint_pose[4], joint_pose[5], joint_pose[6]);
 		}
+		return all_correct;
 	}
 
 };
--- src/my_interesting_moves/src/interesting_moves_example.cpp
+++ src/my_interesting_moves/src/interesting_moves_example.cpp
@@ -8,20 +8,19 @@
 
 #include <my_interesting_moves/my_interesting_moves.hpp>
 
-const double sleep_time = 3;
+const double sleep_time = 3;							//sleep time before each movement apply
+const double my_pose[] = {0, 0, 0, 0, 0, 0, 0};
+const double impossible_pose[] = {4, -5, 6, -7, 8, -9, 10};
 
 int main(int argc, char** argv) {
-	double my_pose[] = {0, 0, 0, 0, 0, 0, 0};
-	double impossible_pose[] = {4, -5, 6, -7, 8, -9, 10};
-	unsigned int angle = 0;
-	double amount;
-	ros::init(argc, argv, "traj_action_client_node"); // name this node
-	ros::NodeHandle nh; //standard ros node handle
-	Baxter_right_arm right_arm(&nh);
+	
+	ros::init(argc, argv, "traj_action_client_node");	//name this node
+	ros::NodeHandle nh;									//standard ros node handle
+	Baxter_right_arm right_arm(&nh);					//create our object
 
-	ROS_WARN("I getting my arm above the table");
-	ros::Duration(sleep_time).sleep();
-	right_arm.move_above_table();
+	ROS_WARN("I getting my arm above the table");		//use WARN message because it is more obvious
+	ros::Duration(sleep_time).sleep();					//wait for 3 second each time so that you can read the text
+	right_arm.move_above_table();						//do the work
 
 	ROS_WARN("I am going to push the beer off the table");
 	ros::Duration(sleep_time).sleep();
@@ -31,7 +30,7 @@ int main(int argc, char** argv) {
 			my_pose[0], my_pose[1], my_pose[2], my_pose[3], 
 			my_pose[4], my_pose[5], my_pose[6]);
 	ros::Duration(sleep_time).sleep();
-	right_arm.move(my_pose);
+	right_arm.move(my_pose);							//this is the normal way of move the arm
 
 
 	ROS_WARN("I am going to wave my right hand 3 times");
@@ -42,25 +41,32 @@ int main(int argc, char** argv) {
 			impossible_pose[0], impossible_pose[1], impossible_pose[2], impossible_pose[3], 
 			impossible_pose[4], impossible_pose[5], impossible_pose[6]);
 	ros::Duration(sleep_time).sleep();
-	right_arm.add_movement(impossible_pose);
-	right_arm.start_move();
-	if (right_arm.wait_for_finish(10.0) == false)
+	//here is another approach of move the arm
+	right_arm.add_movement(impossible_pose);			//add the movement to the sequence
+	right_arm.start_move();								//start moving
+	if (right_arm.wait_for_finish(10.0) == false)		//wait for finish for 10 seconds
 	{
-		ROS_WARN("I did not finished on time, I will stop");
-		right_arm.stop_move();
+		ROS_WARN("I did not finished on time, I will stop");	//if not finished
+		right_arm.stop_move();							//stop moving
 	}
 
 	ros::Duration(sleep_time).sleep();
 	ROS_WARN("Let's try again, I will move back");
-	right_arm.move(my_pose);
+	right_arm.move(my_pose);							//just move to somewhere
 
 	ROS_WARN("I will introduce pose limitation this time");
-	right_arm.enable_pose_limit = true;
+	right_arm.enable_pose_limit = true;					//enable the pose limit so that the arm won't goes to a weird pose
 	ROS_WARN("I am moving to an impossible pose [%f, %f, %f, %f, %f, %f, %f]", 
 			impossible_pose[0], impossible_pose[1], impossible_pose[2], impossible_pose[3], 
 			impossible_pose[4], impossible_pose[5], impossible_pose[6]);
-	ros::Duration(2).sleep();
-	right_arm.move(impossible_pose);
+	ros::Duration(sleep_time).sleep();
+	right_arm.move(impossible_pose);					//try the same pose again
+	ROS_WARN("Now I have reach the maximum pose");
+	
+	ros::Duration(sleep_time).sleep();
+	ROS_WARN("That is all, thank you");
+	right_arm.wave_hand(5);								//I love this motion
+	
 	return 0;
 }
 
