765313c6937548f0dac1681e77d265cbabdd8317
TuZZiX <tianshipei5@126.com>
2016-04-11 15:37:43 -0400

    update

--- baxter.sh
+++ baxter.sh
@@ -19,12 +19,12 @@ unset your_hostname
 # will be overwritten by any variables set here.
 
 # Specify Baxter's hostname
-baxter_hostname="baxter01"
+baxter_hostname="baxter01.case.edu"
 
 # Set *Either* your computers ip address or hostname. Please note if using
 # your_hostname that this must be resolvable to Baxter.
-your_ip="atlas1"
-#your_hostname="my_computer.local"
+#your_ip="192.168.XXX.XXX"
+your_hostname="atlas5.case.edu"
 
 # Specify ROS distribution (e.g. indigo, hydro, etc.)
 ros_version="indigo"
@@ -181,3 +181,4 @@ rm -f -- "${tf}"
 trap - EXIT
 
 # vim: noet
+
