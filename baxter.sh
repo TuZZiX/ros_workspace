b949b91b44c71ef18cc79539c5cd010cb23cb53c
SpecTian <tianshipei5@126.com>
2016-05-01 02:44:23 -0400

    update

--- baxter.sh
+++ baxter.sh
@@ -24,7 +24,7 @@ baxter_hostname="baxter01.case.edu"
 # Set *Either* your computers ip address or hostname. Please note if using
 # your_hostname that this must be resolvable to Baxter.
 #your_ip="192.168.XXX.XXX"
-your_hostname="atlas5.case.edu"
+your_hostname="atlas1.case.edu"
 
 # Specify ROS distribution (e.g. indigo, hydro, etc.)
 ros_version="indigo"
